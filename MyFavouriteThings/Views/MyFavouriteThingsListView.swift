1f068573408626bf6d3335be8b3c37fa6b467979
Courage Zeng <czeng@lcs.on.ca>
2024-11-22 09:48:23 -0500

    created a list that iterate over everything in my array, edited the size of the picture and made it smaller.

--- MyFavouriteThings/Views/MyFavouriteThingsListView.swift
+++ MyFavouriteThings/Views/MyFavouriteThingsListView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct MyFavouriteThingsListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(things) { currentThings in
+                ThingsView(providedThings: currentThings)
+            }
+            .navigationTitle("My favourite things")
+        }
     }
 }
 
--- MyFavouriteThings/Views/ThingsView.swift
+++ MyFavouriteThings/Views/ThingsView.swift
@@ -16,7 +16,7 @@ struct ThingsView: View {
             Image(providedThings.image)
                 .resizable()
                 .scaledToFill()
-                .frame(width: 100, height: 100, alignment: .center)
+                .frame(width: 80, height: 80, alignment: .center)
                 .clipped()
             VStack(alignment: .leading) {
                 Text(providedThings.name)
