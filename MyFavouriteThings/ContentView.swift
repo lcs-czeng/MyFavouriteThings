4ba2c23e318af40c172261c7141063c26ab0245f
Courage Zeng <czeng@lcs.on.ca>
2024-11-22 08:53:10 -0500

    Initial Commit

---
+++ MyFavouriteThings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFavouriteThings
+//
+//  Created by 曾梓恒 on 2024/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFavouriteThings/MyFavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavouriteThingsApp.swift
+//  MyFavouriteThings
+//
+//  Created by 曾梓恒 on 2024/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
